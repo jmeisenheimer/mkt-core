f888d0c77306bec947ce6b235462d8c2b7029e88
Jon Meisenheimer <jonny.meis@gmail.com>
2017-10-28 18:29:37 -0400

    Added files + changed namespace

---
+++ MarketLibrary/Core/Tenor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketLibrary.Core
+{
+    public enum TenorOffsetType
+    {
+        Relative,
+        Absolute
+    }
+
+    public class Tenor
+    {
+        public string Name { get; private set; }
+        
+    }
+}
