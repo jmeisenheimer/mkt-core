38b3feb07176677a4caea42293acd197a8522ccc
Jon Meisenheimer <jonny.meis@gmail.com>
2017-10-28 18:29:37 -0400

    Added files + changed namespace

--- MarketLibrary/Instrument.cs
+++ MarketLibrary/Instrument.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestGC
+namespace MarketLibrary
 {
     public class Instrument
     {
         public string TickerSymbol { get; private set; }
+        public Tenor Tenor { get; private set; }
 
         public Instrument(string tickerSymbol)
         {
